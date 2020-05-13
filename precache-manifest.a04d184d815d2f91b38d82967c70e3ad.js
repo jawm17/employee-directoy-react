0c63c11c210db14ddcf248ae339ba6f6705d48fc
Michael Sanderson <jawm17@gmail.com>
2020-05-13 18:13:32 -0500

    Updates

---
+++ precache-manifest.a04d184d815d2f91b38d82967c70e3ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b3df6247d18fdefecf65fe79a07009f",
+    "url": "/employee-directoy-react/index.html"
+  },
+  {
+    "revision": "23e0370616464a24aea6",
+    "url": "/employee-directoy-react/static/js/2.f80b825a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directoy-react/static/js/2.f80b825a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2fa72f6cf5026acdcbe",
+    "url": "/employee-directoy-react/static/js/main.21fdcbf3.chunk.js"
+  },
+  {
+    "revision": "6197d70ce9c64911f3cf",
+    "url": "/employee-directoy-react/static/js/runtime-main.e3c38734.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/employee-directoy-react/precache-manifest.e57b5ca08c3ab4842add4a88fccb2c89.js"
+  "/employee-directoy-react/precache-manifest.a04d184d815d2f91b38d82967c70e3ad.js"
 );
 
 self.addEventListener('message', (event) => {
